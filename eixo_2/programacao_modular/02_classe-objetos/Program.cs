3743bd25eb9d2cfc1b9646ca8c044efa4694e024
alves1503 <henriquefdalves15@gmail.com>
2025-09-05 00:44:52 -0300

    Fiz a parte de construtores 'pessoa' em csharp, pucminas

--- eixo_2/programacao_modular/02_classe-objetos/Program.cs
+++ eixo_2/programacao_modular/02_classe-objetos/Program.cs
@@ -1,9 +1,10 @@
 ﻿using _02_classe_objetos.models;
 
+//Construtor em default
 Pessoa pessoa1 = new Pessoa();
+pessoa1.exibirInformacao();
 
-pessoa1.nome = "Henrique Alves Gonçalves";
-pessoa1.endereco = "Rua alguma coisa";
-pessoa1.numero = "(99) 99999-9999";
+//Construtor com parametros
+Pessoa pessoa2 = new Pessoa("Henrique Alves Gonçalves", "Rua 1", "(99) 99999-9999");
+pessoa2.exibirInformacao();
 
-pessoa1.primeiroNome();
--- eixo_2/programacao_modular/02_classe-objetos/models/Pessoa.cs
+++ eixo_2/programacao_modular/02_classe-objetos/models/Pessoa.cs
@@ -9,14 +9,37 @@ namespace _02_classe_objetos.models
     public class Pessoa
     {
 
-        public string nome;
-        public string endereco;
-        public string numero;
+        public string Nome;
+        public string Endereco;
+        public string Numero;
 
-        public void primeiroNome()
+        public Pessoa(string nome, string endereco, string numero)
         {
-            Console.WriteLine($"Seja bem vindo {nomeCompleto}");
+            this.Nome = nome;
+            this.Endereco = endereco;
+            this.Numero = numero;
         }
-        
+
+        public Pessoa()
+        {
+            this.Nome = "Maria Clara";
+            this.Endereco = "Rua 2";
+            this.Numero = "(11) 11111-1111";
+        }
+
+
+        public string PrimeiroNome()
+        {
+            return Nome.Split(' ')[0]; // retorna só o primeiro nome
+        }
+
+        public void exibirInformacao()
+        {
+            Console.WriteLine($"Seja bem vindo: {Nome}");
+            Console.WriteLine($"Seu endereço: {Endereco}");
+            Console.WriteLine($"Seu telefone: {Numero}");
+        }
+
+
     }
 }
